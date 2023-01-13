a9443510dc39080512db436b0dd79573cafcf7da
Victor-Simon1 <forzaitalia124@gmail.com>
2023-01-13 09:53:32 +0100

    :tada: initialize project

---
+++ Source/Steer/SteerGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SteerGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class STEER_API ASteerGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
